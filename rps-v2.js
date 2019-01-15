2f494b3970d7a03395d37976f3b3301064382cbb
bycdiaz <cdiaz86@gmail.com>
2019-01-15 13:17:37 -0500

    added move comparison function in .js and div for containing result in .html.

--- rps-v2.js
+++ rps-v2.js
@@ -24,12 +24,20 @@ window.onload = function() {
       // appends CPU move to HTML
       const cpuMoveDisplay = document.querySelector('#cpuPick')
 
-      const cpuMove = document.createElement('p')
+      const cpuMove = document.createElement('p');
       cpuMove.textContent = computerPlay();
       cpuMove.classList.add('onClickcpuDisplay');
 
       cpuMoveDisplay.appendChild(cpuMove);
       console.log("CPU selected: " + computerPlay() + ".");
+
+      // appends comparison to HTML
+      const comparisonDisplay = document.querySelector("#roundOutcome")
+
+      const movesCompared = document.createElement('p');
+      movesCompared.textContent = compareMoves;
+
+      comparisonDisplay.appendChild(movesCompared);
 
       });
     });
@@ -51,5 +59,47 @@ function computerPlay() {
         return 'scissors';
     }
     const cpuSelection = computerPlay();
+
+}
+
+function playRound(userSelection,cpuSelection) {
+
+
+    // round outcomes
+    if (userSelection === "rock") {
+        if (cpuSelection === "paper") {
+            cpuScore++;
+            return "Paper beats rock. You lose!";
+        } else if (cpuSelection === "scissors") {
+            userScore++;
+            return "Rock beats scissors. You win!";
+        } else if (userSelection === cpuSelection) {
+            return "Rock can't beat rock. This is a draw.";
+        }
+
+    } else if (userSelection === "paper") {
+        if (cpuSelection === "rock") {
+            userScore++;
+            return "Paper beats rock. You win!"
+        } else if (cpuSelection === "scissors") {
+            cpuScore++;
+            return "Scissors beats paper. You lose!"
+        } else if (userSelection === cpuSelection) {
+            return "Paper can't beat paper. This is a draw."
+        }
+
+    } else if (userSelection === "scissors") {
+        if (cpuSelection === "rock") {
+            cpuScore++;
+            return "Rock beats scissors. You lose!"
+        } else if (cpuSelection === "paper") {
+            userScore++;
+            return "Scissors beats paper. You win!"
+        } else if (userSelection === cpuSelection) {
+            return "Scissors can't beat scissors. This is a draw."
+        }
+    }
+
+    const compareMoves = playRound();
 
 }
